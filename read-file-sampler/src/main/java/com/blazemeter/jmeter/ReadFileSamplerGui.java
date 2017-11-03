30ec48e4d59f441d383507cede52afb994593b27
Artem Fedorov <artem.fedorov@blazemeter.com>
2017-11-03 13:52:51 +0300

    add gui elements, implements clearGui

--- read-file-sampler/src/main/java/com/blazemeter/jmeter/ReadFileSamplerGui.java
+++ read-file-sampler/src/main/java/com/blazemeter/jmeter/ReadFileSamplerGui.java
@@ -1,9 +1,72 @@
 package com.blazemeter.jmeter;
 
+import kg.apc.jmeter.JMeterPluginsUtils;
+import kg.apc.jmeter.gui.BrowseAction;
+import kg.apc.jmeter.gui.GuiBuilderHelper;
 import org.apache.jmeter.samplers.gui.AbstractSamplerGui;
 import org.apache.jmeter.testelement.TestElement;
 
+import javax.swing.*;
+import java.awt.*;
+
 public class ReadFileSamplerGui extends AbstractSamplerGui {
+
+    // TODO: use full URL and change cmn version to 0.6 after it has been released
+    public static final String WIKIPAGE = "ReadFileSampler";
+
+    private JTextField filenameField;
+    private JButton browseButton;
+
+
+    public ReadFileSamplerGui() {
+        initGui();
+        initGuiValues();
+    }
+
+    private void initGui() {
+        setLayout(new BorderLayout(0, 5));
+        setBorder(makeBorder());
+
+        Container topPanel = makeTitlePanel();
+
+        add(JMeterPluginsUtils.addHelpLinkToPanel(topPanel, WIKIPAGE), BorderLayout.NORTH);
+        add(topPanel, BorderLayout.NORTH);
+
+        JPanel mainPanel = new JPanel(new GridBagLayout());
+
+        GridBagConstraints labelConstraints = new GridBagConstraints();
+        labelConstraints.anchor = GridBagConstraints.FIRST_LINE_END;
+
+        GridBagConstraints editConstraints = new GridBagConstraints();
+        editConstraints.anchor = GridBagConstraints.FIRST_LINE_START;
+        editConstraints.weightx = 1.0;
+        editConstraints.fill = GridBagConstraints.HORIZONTAL;
+
+        int row = 0;
+        addToPanel(mainPanel, labelConstraints, 0, row, new JLabel("Filename: ", JLabel.RIGHT));
+        addToPanel(mainPanel, editConstraints, 1, row, filenameField = new JTextField(20));
+        addToPanel(mainPanel, labelConstraints, 2, row, browseButton = new JButton("Browse..."));
+        GuiBuilderHelper.strechItemToComponent(filenameField, browseButton);
+
+        editConstraints.insets = new java.awt.Insets(2, 0, 0, 0);
+        labelConstraints.insets = new java.awt.Insets(2, 0, 0, 0);
+
+        browseButton.addActionListener(new BrowseAction(filenameField, true));
+
+        JPanel container = new JPanel(new BorderLayout());
+        container.add(mainPanel, BorderLayout.NORTH);
+        add(container, BorderLayout.CENTER);
+    }
+
+    private void initGuiValues() {
+        filenameField.setText("");
+    }
+
+    private void addToPanel(JPanel panel, GridBagConstraints constraints, int col, int row, JComponent component) {
+        constraints.gridx = col;
+        constraints.gridy = row;
+        panel.add(component, constraints);
+    }
 
     @Override
     public String getStaticLabel() {
@@ -30,5 +93,6 @@ public class ReadFileSamplerGui extends AbstractSamplerGui {
     @Override
     public void clearGui() {
         super.clearGui();
+        initGuiValues();
     }
 }
